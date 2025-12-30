8c136be9caa5888bcb6e32fd5112c11072bff85f
Victor Silot <victorsilotdev@gmail.com>
2025-12-30 09:36:24 -0400

    Implementando cliente HTTP

--- backend/src/main/java/com/hackathon/SentimentAPI/client/MlServiceClient.java
+++ backend/src/main/java/com/hackathon/SentimentAPI/client/MlServiceClient.java
@@ -1,5 +1,28 @@
 package com.hackathon.SentimentAPI.client;
 
+import com.hackathon.SentimentAPI.dto.MlServiceResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Component
 public class MlServiceClient {
-    
+
+    @Value("${ml.service.url}")
+    private String mlServiceUrl;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public MlServiceResponse predict(String text) {
+
+        Map<String, String> request = Map.of("text", text);
+
+        return restTemplate.postForObject(
+                mlServiceUrl,
+                request,
+                MlServiceResponse.class
+        );
+    }
 }
