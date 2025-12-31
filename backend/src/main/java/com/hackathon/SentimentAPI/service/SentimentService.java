f16a64c7d8426d8e22514d16c05e3e5eb75a69a5
Victor Silot <victorsilotdev@gmail.com>
2025-12-31 11:06:54 -0400

    Conectando SentimentService ao MlServiceClient

--- backend/src/main/java/com/hackathon/SentimentAPI/service/SentimentService.java
+++ backend/src/main/java/com/hackathon/SentimentAPI/service/SentimentService.java
@@ -1,5 +1,7 @@
 package com.hackathon.SentimentAPI.service;
 
+import com.hackathon.SentimentAPI.client.MlServiceClient;
+import com.hackathon.SentimentAPI.dto.MlServiceResponse;
 import org.springframework.stereotype.Service;
 import com.hackathon.SentimentAPI.dto.SentimentRequest;
 import com.hackathon.SentimentAPI.dto.SentimentResponse;
@@ -7,9 +9,19 @@ import com.hackathon.SentimentAPI.dto.SentimentResponse;
 @Service
 public class SentimentService {
 
+    private final MlServiceClient mlServiceClient;
+
+    public SentimentService(MlServiceClient mlServiceClient) {
+        this.mlServiceClient = mlServiceClient;
+    }
+
     public SentimentResponse analisar(SentimentRequest request) {
 
-        // TEMPORAL (simulação de regra de negócio de classificação de sentimento da revin)
-        return new SentimentResponse("Positivo", 0.95);
+        MlServiceResponse mlResponse = mlServiceClient.predict(request.getText());
+
+        return new SentimentResponse(
+                mlResponse.getPrevisao(),
+                mlResponse.getProbabilidade()
+        );
     }
 }
