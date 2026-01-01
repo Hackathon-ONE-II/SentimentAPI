4f17e274453d60f8c4d4542990422ec4a26a00c2
Rick <rick1511santos@gmail.com>
2026-01-01 19:32:38 -0300

    Update SentimentRequest.java
    inclusão de size max no texto, podendo facilitar e melhorar a precisão da analise e utilização de text.trim para remover espaços em branco

--- backend/src/main/java/com/hackathon/SentimentAPI/dto/SentimentRequest.java
+++ backend/src/main/java/com/hackathon/SentimentAPI/dto/SentimentRequest.java
@@ -1,12 +1,23 @@
 package com.hackathon.SentimentAPI.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record SentimentRequest(
 
-        @NotBlank(message = "O texto é obrigatório")
-        @Size(min = 5, message = "O texto deve ter pelo menos 5 caracteres")
+public record SentimentRequest(
+        // validação para o campo text min e max size
+        @NotBlank(message = "{sentiment.text.notBlank}")
+        @Size(min = 5, max = 1300, message = "{sentiment.text.size}")
+        @Pattern(
+                regexp = ".*[a-zA-Z].*",
+                message = "O texto deve conter pelo menos uma palavra"
+        )
         String text
 
-) {}
+) {
+        // trim para remover espaços em branco no início e no fim
+        public SentimentRequest {
+                text = text.trim();
+        }
+}
