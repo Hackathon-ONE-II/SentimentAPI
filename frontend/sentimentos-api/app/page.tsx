b9c1cb7618610cb0773249ff69e2e890a946e14b
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2026-01-07 14:12:53 -0300

    feat: adição de mais funcionalidades no frontend

--- frontend/sentimentos-api/app/page.tsx
+++ frontend/sentimentos-api/app/page.tsx
@@ -3,8 +3,11 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Teste
+          <h1 className="max-w-xs text-3xl font-semibold leading-50 tracking-tight text-black dark:text-zinc-50n m-1">
+            Entenda o sentimento
+          </h1>
+          <h1 className="max-w-xs text-3xl font-semibold leading-50 bg-linear-to-r from-blue-400 to-indigo-500 bg-clip-text text-transparent">
+            dos seus feedbacks
           </h1>
       </main>
     </div>
